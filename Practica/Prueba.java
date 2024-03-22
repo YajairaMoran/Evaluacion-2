8ffbce90caf9b99dfe3d649849be8eebbe7955db
YajairaMoran <159494447+YajairaMoran@users.noreply.github.com>
2024-03-21 18:44:43 -0600

    Add files via upload

---
+++ Practica/Prueba.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Prueba {
+       public static void main(String[] args) {
+        Scanner cap = new Scanner (System.in);
+         System.out.println("Escribe tu nombre: ");
+         Sring nombre = captura.nextLine();
+         System.out.println(nombre);
+      }
+}
