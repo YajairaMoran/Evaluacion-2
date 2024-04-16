f063b0176875ca17ff6a28ddd2b6a8c1599baa85
YajairaMoran <159494447+YajairaMoran@users.noreply.github.com>
2024-04-16 16:28:25 -0600

    Add files via upload

---
+++ EVA2_19_FOR_2/src/eva2_19_for_2/EVA2_19_FOR_2.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_19_for_2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author danie
+ */
+public class EVA2_19_FOR_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //preguntar: cantidad de estudiantes del grupo
+        int numAlum;
+        Scanner captu = new Scanner (System.in);
+        System.out.println("¿Cuantos estudiantes tiene el grupo?");
+        numAlum = captu.nextInt();
+        //Hay que capturar todas las calificaciones
+                    //   i=1           5
+            //<= incluye el 5 y el < lo excluye //ambos hacen lo mismo
+        for (int i = 1; i <= numAlum; i++) {
+            System.out.println("Calificacion");
+            int calif = captu.nextInt();
+        }
+
+            
+            
+            
+            
+            
+        }
+{
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
