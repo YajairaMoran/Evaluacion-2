e78b8b1f39866d74ca58462d9bd4fcc112aa2969
YajairaMoran <159494447+YajairaMoran@users.noreply.github.com>
2024-04-16 16:32:21 -0600

    Add files via upload

---
+++ EVA2_22_DO_WHILE/src/eva2_22_do_while/EVA2_22_DO_WHILE.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_22_do_while;
+
+/**
+ *
+ * @author danie
+ */
+public class EVA2_22_DO_WHILE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int cont = 1;
+        do{
+            System.out.print(cont + " - ");
+            cont++;
+        }while (cont <= 10);
+        
+    }
+    
+}
